0c112a9dc14ea9937d7c5b0fdc957d709c716b53
Krishna <krishnapdbashyal23@gmail.com>
2025-11-29 15:21:39 -0500

    fixed some typos and syntax errors. code working yayy

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -2,11 +2,8 @@ import { useState } from "react";
 
 function App() {
   const [name, setName] = useState("");
-  const [imageUrl, setImalgeUrl] = useSate(null);
+  const [imageUrl, setImageUrl] = useState(null);
   const [error, setError] = useState("");
-  const cors= require("cors");
-  app.use(cors());
-
   const handleSearch = async () => {
     if (!name) {
       setError("Please enter a character name");
@@ -15,7 +12,7 @@ function App() {
 
     // Clear previous state
     setError("");
-    setImalgeUrl(null);
+    setImageUrl(null);
     // build request url
     const url = `http://localhost:3001/api/getImage?name=${name}`;
 
@@ -27,32 +24,32 @@ function App() {
         return;
       }
       const blob = await response.blob();
-      const imgUrl = url.createObjectURL(blob);
-      setImalgeUrl(imgURL);
+      const imgUrl = URL.createObjectURL(blob);
+      setImageUrl(imgUrl);
     } catch (err) {
       setError("Server error");
     }
   };
   return (
-    <div> style={{padding: "30 px", fontFamily: "Arial"}}
-    <h1> Image Search </h1>
-    <input
-      type="text"
-      placeholder="Enter name ( tom, jerry, dog)"
-      value ={name}
-      
-      onChange={(e)=> setName(e.target.value)}
-      style={{padding:"8px", marginRight:"10px"}}
+    <div style={{ padding: "30px", fontFamily: "Arial" }}>
+      <h1> Image Search </h1>
+      <input
+        type="text"
+        placeholder="Enter name ( tom, jerry, dog)"
+        value={name}
+
+        onChange={(e) => setName(e.target.value)}
+        style={{ padding: "8px", marginRight: "10px" }}
       />
 
-      <button onClick ={handleSearch} style={{padding:"8px 16px"}}>Search</button>
-      {error && <p style={{color:"red", marginTop: "20px"}}>{error}</p>}
-    {imageUrl &&(
-      <div stule={{marginTop: "20pxx"}}>
-        <h3>Result:</h3>
-        <img src={imageUrl} alt="Character" width='250'/>
-    </div>
-    )}
+      <button onClick={handleSearch} style={{ padding: "8px 16px" }}>Search</button>
+      {error && <p style={{ color: "red", marginTop: "20px" }}>{error}</p>}
+      {imageUrl && (
+        <div style={{ marginTop: "20px" }}>
+          <h3>Result:</h3>
+          <img src={imageUrl} alt="Character" width='250' />
+        </div>
+      )}
     </div>
   );
 }
